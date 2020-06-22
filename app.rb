2924cbf4c81ba2d5346fc793585c8dedcaf132e6
Jashema Panapa <jcpanapa@gmail.com>
2020-06-22 06:58:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,22 +6,40 @@ module FormsLab
     # code other routes/actions here
     
      get '/' do
-      erb :root
+
+     erb :root
+
     end
+
+
 
     get '/new' do
-      erb :'pirates/new'
+
+     erb :"pirates/new"
+
     end
 
+
+
+
+
     post '/pirates' do
-     @pirate = Pirate.new(params[:pirate])
 
-     params[:pirate][:ships].each do |details|
-       Ship.new(details)
-     end
-     @ships = Ship.all
+      @pirate = Pirate.new(params[:pirate])
 
-     erb :'pirates/show'
-   end
+      params[:pirate][:ships].each do |details|
+
+        Ship.new(details)
+
+      end
+
+      @ships = Ship.all
+
+
+
+      erb :'pirates/show'
+
+    end
+    
   end
 end
